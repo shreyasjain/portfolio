9c5930899dd119ff50a6ead305e64b602560f10f
shreyasjain <13sjain4ur7@gmail.com>
2021-03-10 22:30:49 +0530

    changes made

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import "../Styles/ProjectCard.scss"
 
+
 function ProjectCard(props) {
+
+    const handleClick=(e)=>{
+        e.preventDefault()
+        window.location.href=props.github
+    }
+
     return (
         <div>
-            <div className="project-card-container">
-                <div style={{"background-image":"url(https://source.unsplash.com/random)"}} className="project-card-container-image">
+            <div className="project-card-container" onClick={handleClick}>
+                <div style={{"background-image":`url(${props.link})`}} className="project-card-container-image">
 
                 </div>
                 <div className="project-card-container-details">
--- src/Containers/Experience.js
+++ src/Containers/Experience.js
@@ -13,7 +13,7 @@ function Experience() {
                     <ServiceCard title="CSS3" imageLink="https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/121-css3-512.png"/>
                     <ServiceCard title="JAVASCTIPT" imageLink="https://www.freepnglogos.com/uploads/javascript-png/javascript-vector-logo-yellow-png-transparent-javascript-vector-12.png"/>
                     <ServiceCard title="REACT.JS" imageLink="https://lh3.googleusercontent.com/proxy/xN3N_RDZiot94mVSM0LEv78nlfOx1wvOR2pnAmk0O_cx0Q1pKHASKcGD2xXpkQGUQlKOysJmI3buTvcl2BWOVM9PfZVNAcPFyy_5AQ7J7R47UE0=s0-d"/>
-                    <ServiceCard title="JAVA" imageLink="https://assets.stickpng.com/images/58480979cef1014c0b5e4901.png"/>
+                    <ServiceCard title="JAVA" imageLink="http://assets.stickpng.com/images/58480979cef1014c0b5e4901.png"/>
                     <ServiceCard title="SPRING" imageLink="https://cdn.freebiesupply.com/logos/large/2x/spring-3-logo-png-transparent.png"/>
                     <ServiceCard title="SQL" imageLink="https://icons-for-free.com/iconfiles/png/512/logo+my+query+server+sql+icon-1320184811372606623.png"/>
                     <ServiceCard title="FIREBASE" imageLink="https://cdn4.iconfinder.com/data/icons/google-i-o-2016/512/google_firebase-2-512.png"/>
